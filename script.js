c2a49dd479107e79eff83d2c8459cfdf0b949cdb
UCAPIP <ucapip@gmail.com>
2023-02-14 13:06:42 +0300

    Настроил кнопки скрытия контента на JS

---
+++ script.js
@@ -0,0 +1,133 @@
+function tab(but_tab, hidden_block_tab) {
+  	/*проверяем на событие нажатия на кнопку которыю вы передаёте в качестве параметра*/
+    but_tab.addEventListener("click", function() {
+      	/*если у блока с контентом есть класс "hidden_block"*/
+        if(hidden_block_tab.classList.contains('hidden_block')) {
+          	/*удаляем класс "hidden_block"*/
+            hidden_block_tab.classList.remove('hidden_block');
+          	/*ставим класс "visible_block"*/
+            hidden_block_tab.classList.add('visible_block');
+        }
+      	/*если у блока с контентом есть класс "visible_block"*/
+        else if(hidden_block_tab.classList.contains('visible_block')) {
+            hidden_block_tab.classList.remove('visible_block');
+            hidden_block_tab.classList.add('hidden_block');
+        }
+    }); 
+}
+
+/*находим элемент кнопку*/
+var download_more = document.querySelector('.end-plans-tabs .menu_1');
+/*находим элемент блока со скрытым контентом*/
+var hidden_list = document.querySelector('.end-plans-tabs .tab_content_1');
+  
+/*запускаем функцию и передаём в неё параметры*/
+tab(download_more, hidden_list);
+
+
+
+
+function tab(but_tab, hidden_block_tab) {
+  	/*проверяем на событие нажатия на кнопку которыю вы передаёте в качестве параметра*/
+    but_tab.addEventListener("click", function() {
+      	/*если у блока с контентом есть класс "hidden_block"*/
+        if(hidden_block_tab.classList.contains('hidden_block')) {
+          	/*удаляем класс "hidden_block"*/
+            hidden_block_tab.classList.remove('hidden_block');
+          	/*ставим класс "visible_block"*/
+            hidden_block_tab.classList.add('visible_block');
+        }
+      	/*если у блока с контентом есть класс "visible_block"*/
+        else if(hidden_block_tab.classList.contains('visible_block')) {
+            hidden_block_tab.classList.remove('visible_block');
+            hidden_block_tab.classList.add('hidden_block');
+        }
+    }); 
+}
+
+/*находим элемент кнопку*/
+var download_more = document.querySelector('.end-plans-tabs .menu_2');
+/*находим элемент блока со скрытым контентом*/
+var hidden_list = document.querySelector('.end-plans-tabs .tab_content_2');
+  
+/*запускаем функцию и передаём в неё параметры*/
+tab(download_more, hidden_list);
+
+
+function tab(but_tab, hidden_block_tab) {
+  	/*проверяем на событие нажатия на кнопку которыю вы передаёте в качестве параметра*/
+    but_tab.addEventListener("click", function() {
+      	/*если у блока с контентом есть класс "hidden_block"*/
+        if(hidden_block_tab.classList.contains('hidden_block')) {
+          	/*удаляем класс "hidden_block"*/
+            hidden_block_tab.classList.remove('hidden_block');
+          	/*ставим класс "visible_block"*/
+            hidden_block_tab.classList.add('visible_block');
+        }
+      	/*если у блока с контентом есть класс "visible_block"*/
+        else if(hidden_block_tab.classList.contains('visible_block')) {
+            hidden_block_tab.classList.remove('visible_block');
+            hidden_block_tab.classList.add('hidden_block');
+        }
+    }); 
+}
+
+/*находим элемент кнопку*/
+var download_more = document.querySelector('.end-plans-tabs .menu_3');
+/*находим элемент блока со скрытым контентом*/
+var hidden_list = document.querySelector('.end-plans-tabs .tab_content_3');
+  
+/*запускаем функцию и передаём в неё параметры*/
+tab(download_more, hidden_list);
+
+function tab(but_tab, hidden_block_tab) {
+  	/*проверяем на событие нажатия на кнопку которыю вы передаёте в качестве параметра*/
+    but_tab.addEventListener("click", function() {
+      	/*если у блока с контентом есть класс "hidden_block"*/
+        if(hidden_block_tab.classList.contains('hidden_block')) {
+          	/*удаляем класс "hidden_block"*/
+            hidden_block_tab.classList.remove('hidden_block');
+          	/*ставим класс "visible_block"*/
+            hidden_block_tab.classList.add('visible_block');
+        }
+      	/*если у блока с контентом есть класс "visible_block"*/
+        else if(hidden_block_tab.classList.contains('visible_block')) {
+            hidden_block_tab.classList.remove('visible_block');
+            hidden_block_tab.classList.add('hidden_block');
+        }
+    }); 
+}
+
+/*находим элемент кнопку*/
+var download_more = document.querySelector('.end-plans-tabs .menu_4');
+/*находим элемент блока со скрытым контентом*/
+var hidden_list = document.querySelector('.end-plans-tabs .tab_content_4');
+  
+/*запускаем функцию и передаём в неё параметры*/
+tab(download_more, hidden_list);
+
+function tab(but_tab, hidden_block_tab) {
+  	/*проверяем на событие нажатия на кнопку которыю вы передаёте в качестве параметра*/
+    but_tab.addEventListener("click", function() {
+      	/*если у блока с контентом есть класс "hidden_block"*/
+        if(hidden_block_tab.classList.contains('hidden_block')) {
+          	/*удаляем класс "hidden_block"*/
+            hidden_block_tab.classList.remove('hidden_block');
+          	/*ставим класс "visible_block"*/
+            hidden_block_tab.classList.add('visible_block');
+        }
+      	/*если у блока с контентом есть класс "visible_block"*/
+        else if(hidden_block_tab.classList.contains('visible_block')) {
+            hidden_block_tab.classList.remove('visible_block');
+            hidden_block_tab.classList.add('hidden_block');
+        }
+    }); 
+}
+
+/*находим элемент кнопку*/
+var download_more = document.querySelector('.end-plans-tabs .menu_5');
+/*находим элемент блока со скрытым контентом*/
+var hidden_list = document.querySelector('.end-plans-tabs .tab_content_5');
+  
+/*запускаем функцию и передаём в неё параметры*/
+tab(download_more, hidden_list);
